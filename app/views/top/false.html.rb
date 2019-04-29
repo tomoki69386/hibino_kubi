5381ffa0e64cdd66c94ab1b6335476765ca1be6e
tomoki69386 <tomoki_69386@yahoo.co.jp>
2019-04-29 19:17:22 +0900

    Update

--- app/views/top/false.html.rb
+++
@@ -1,5 +0,0 @@
-<meta name="twitter:card" content="summary_large_image" /> /*①*/
-<meta name="twitter:site" content="@tomoki_sun" /> /*②*/
-<meta property="og:title" content="株式会社グランフェアズ">
-<meta property="og:description" content="グランフェアズの強みは、実践型プロジェクト管理と精緻なサイト設計です。">
-<meta property="og:image" content="http://www.granfairs.com/assets/img/fb-ogp-01.png">
