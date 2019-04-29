42f75867d1bc27fad126a0f326a54ea77bc502ba
tomoki69386 <tomoki_69386@yahoo.co.jp>
2019-04-29 19:06:12 +0900

    Update TopController

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -2,4 +2,12 @@ class TopController < ApplicationController
   def index
     
   end
+
+  def true
+
+  end
+
+  def false
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'top#index'
   get "/result" => "result#show"
+  get "/true" => "top#true"
+  get "/false" => "top#false"
 end
